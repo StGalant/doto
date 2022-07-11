09f077e660692d5245db28d6953e6372c064bed8
Vladimir Y <st.galant@gmail.com>
2022-07-11 07:01:50 +0500

    Дизайн страницы проектов

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -4,8 +4,11 @@ import { useI18n } from 'vue-i18n'
 import { useProjectsStore } from '~/store/projects'
 import type { Project } from '~/models/Project'
 import { createSortFunctions } from '~/composables/createSortFunctions'
+import VButton from '~/components/VButton.vue'
+import VTags from '~/components/VTags.vue'
 
 export default defineComponent({
+  components: { VButton, VTags },
   setup() {
     const { t } = useI18n()
     const sortFunctions = createSortFunctions<Project>(['title', 'createdAt'])
@@ -17,13 +20,11 @@ export default defineComponent({
         fn = (a, b) => -1 * fn(a, b)
       return fn
     })
-
     const store = useProjectsStore()
     store.loadProjects()
     const projects = computed(() => {
       return store.projects.slice().sort(sortFn.value)
     })
-
     return {
       projects,
       sortBy,
@@ -38,16 +39,20 @@ export default defineComponent({
 
 <template>
   <div class="container mx-auto">
-    <RouterLink :to="{ name: 'NewProject' }">
+    <VButton class="my-2" @click="$router.push({ name: 'NewProject' })">
       {{ t('projects.add') }} <div i="carbon-add" />
-    </RouterLink>
+    </VButton>
     <ul v-if="!store?.loading" class="Projects__list">
-      <li v-for="p in projects" :key="p.id" class="Projects__card" data-test-id="project-card">
-        <div class="title">
-          <RouterLink :to="{ name: 'Project', params: { projectId: p.id } }">
-            {{ p.title }}
-          </RouterLink>
+      <li
+        v-for="p in projects" :key="p.id"
+        class="Projects__card"
+        data-test-id="project-card"
+        @click="$router.push({ name: 'Project', params: { projectId: p.id } })"
+      >
+        <div class="text-xl">
+          {{ p.title }}
         </div>
+        <VTags :model-value="p.tags" :color="true" :disabled="true" />
       </li>
     </ul>
     <ul v-if="store?.loading" class="projects projects-stub">
@@ -59,5 +64,20 @@ export default defineComponent({
 </template>
 
 <style scope>
-
+.Projects__list {
+  display: flex;
+  gap: 1rem;
+  flex-wrap: wrap;
+}
+.Projects__card {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 14rem;
+  height: 8rem;
+  border: 2px solid var(--color-brand-0);
+  border-radius: 0.8rem;
+  padding: .5rem;
+  cursor: pointer;
+}
 </style>
