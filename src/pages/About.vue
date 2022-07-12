10909e55d406387ac76daace1fff083b6df386b7
Vladimir Y <st.galant@gmail.com>
2022-07-12 15:12:24 +0500

    About

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Hello</h1>
+  <main class="container mx-auto">
+    <h1 class="text-2xl my-2">
+      DOto - небольшое приложение для ведения задач в стиле Kanban.
+    </h1>
+    <p>Минималистичный дизайн и интуитивно понятный интерфейс не отвлекают от основной работы.</p>
+  </main>
 </template>
