872b010ea29cf48ffd330fed9bd3c892f0b3c74d
Vladimir Y <st.galant@gmail.com>
2022-07-12 18:12:52 +0500

    Add background

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import { useHead } from '@vueuse/head'
 import { isDark, preferredDark } from '~/composables/dark'
 import { useAppInit } from '~/composables/useAppInit'
 import { useUserStore } from '~/store/user'
-
+import Background from '~/components/Background.vue'
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -35,5 +35,20 @@ if (!import.meta.env.SSR) {
 </script>
 
 <template>
+  <div id="app-message" />
+  <Background />
   <RouterView v-if="initialized" />
 </template>
+
+<style>
+ #app-message {
+  position: fixed;
+  top: 0;
+  left: 0;
+  font-weight: bold;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  padding: .25rem;
+ }
+</style>
---
+++ src/components/Background.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import { Motion } from 'motion/vue'
+</script>
+
+<template>
+  <div class="Background fixed inset-0 ">
+    <div
+      v-for="i in 30" :key="i"
+      class="Background__item"
+      :style="{
+        left: `${Math.random() * 90}%`,
+        top: `${Math.random() * 90}%`,
+        transform: `scale(${Math.random() + .1}`,
+        opacity: `${Math.random() * .12 + .1}`,
+      }"
+    />
+  </div>
+</template>
+
+<style>
+.Background {
+  z-index: -1000;
+  filter: blur(4px);
+}
+.Background__item {
+  position: absolute;
+  width: 400px;
+  height: 400px;
+  border: 15px solid rgb(197, 190, 255);
+  background-color: var(--color-background);
+}
+</style>
