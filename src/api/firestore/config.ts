6c99a4038da79518a94bce88ec580b84385c5595
Vladimir Y <st.galant@gmail.com>
2022-07-13 11:31:27 +0500

    firebase settings reset

--- src/api/firestore/config.ts
+++ src/api/firestore/config.ts
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAAwEEeLam8M-bcgIQm_gV5L_2k2bQYuUg',
-  authDomain: 'doto-afa16.firebaseapp.com',
-  projectId: 'doto-afa16',
-  storageBucket: 'doto-afa16.appspot.com',
-  messagingSenderId: '829415098203',
-  appId: '1:829415098203:web:84d6ee0d98e03b9b6e7c7e',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
 }
 
 const app = initializeApp(firebaseConfig)
